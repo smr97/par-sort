7222dbe9c082c2e7b968bf7035e3c9688dfdcd26
smr97 <saurabh.mraje@gmail.com>
2020-02-26 20:25:48 +0100

    added paths for grid submission

--- benchmarks/cpp/run.sh
+++ benchmarks/cpp/run.sh
@@ -3,5 +3,7 @@
 cd $(readlink -f "$(dirname "$0")")
 set -ex
 
-g++ -O2 bench.cpp -o bench -std=c++14 -fopenmp -ltbb -I/usr/include/tbb
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/grid5000/spack/opt/spack/linux-debian9-x86_64/gcc-6.4.0/intel-tbb-2019.2-q2t24fbv6s4d5x2j5jaykuqrdkec75zq/lib
+export LIBRARY_PATH=$LIBRARY_PATH:$LD_LIBRARY_PATH
+g++ -O2 bench.cpp -o bench -std=c++14 -fopenmp -ltbb -I/grid5000/spack/opt/spack/linux-debian9-x86_64/gcc-6.4.0/intel-tbb-2019.2-q2t24fbv6s4d5x2j5jaykuqrdkec75zq/include/
 ./bench
