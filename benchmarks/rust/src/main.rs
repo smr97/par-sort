37ea5249406dc713c945280044337b7acd533816
smr97 <saurabh.mraje@gmail.com>
2020-04-27 01:12:22 +0530

    fixed the dependency issue and added the new sort

--- benchmarks/rust/src/main.rs
+++ benchmarks/rust/src/main.rs
@@ -4,11 +4,13 @@ extern crate par_sort;
 extern crate rand;
 extern crate rayon;
 extern crate rayon_adaptive;
+extern crate rayon_try_fold;
 
 use par_sort::ParallelSliceSort;
 use rand::{thread_rng, Rng};
 use rayon::current_num_threads;
 use rayon_adaptive::merge_sort_adaptive;
+use rayon_try_fold::{iter_par_sort, slice_par_sort};
 use std::time::Instant;
 
 const PROBLEM_SIZE: u64 = 100_000_000;
@@ -49,7 +51,27 @@ fn main() {
         merge_sort_adaptive(&mut v);
         let elapsed = start.elapsed();
         println!(
-            "adaptive sort {} ms",
+            "rayon-adaptive sort {} ms",
+            elapsed.as_secs() * 1000 + elapsed.subsec_nanos() as u64 / 1_000_000
+        );
+    }
+    {
+        let mut v: Vec<u64> = (0..PROBLEM_SIZE).map(|_| rand::random()).collect();
+        let start = Instant::now();
+        slice_par_sort(&mut v);
+        let elapsed = start.elapsed();
+        println!(
+            "try_fold slice sort {} ms",
+            elapsed.as_secs() * 1000 + elapsed.subsec_nanos() as u64 / 1_000_000
+        );
+    }
+    {
+        let mut v: Vec<u64> = (0..PROBLEM_SIZE).map(|_| rand::random()).collect();
+        let start = Instant::now();
+        iter_par_sort(&mut v);
+        let elapsed = start.elapsed();
+        println!(
+            "try_fold slice sort {} ms",
             elapsed.as_secs() * 1000 + elapsed.subsec_nanos() as u64 / 1_000_000
         );
     }
